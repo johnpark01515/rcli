120873d2f57ad4f3b681dee6f61c5135775b36a5
czq <595588383@qq.com>
2024-05-05 20:50:53 +0800

    feature: support toml-http fs server

--- src/process/http_serve.rs
+++ src/process/http_serve.rs
@@ -1,5 +1,3 @@
-use std::{path::PathBuf, sync::Arc};
-
 use anyhow::Result;
 use axum::{
     extract::{Path, State},
@@ -7,20 +5,30 @@ use axum::{
     routing::get,
     Router,
 };
+use std::{path::PathBuf, sync::Arc};
+use tower_http::services::ServeDir;
 use tracing::info;
 
 #[derive(Debug, Clone)]
 struct PathState {
-    // ...
     path: PathBuf,
 }
 
 pub async fn http_process_serve(path: PathBuf, port: u16) -> Result<()> {
     info!("{:?},{}", path, port);
+    let service = ServeDir::new(path.clone());
     let state = Arc::new(PathState { path });
 
     let app = Router::new()
         .route("/*path", get(file_handler))
+        .nest_service(
+            "/tower/",
+            service
+                .precompressed_br()
+                .precompressed_deflate()
+                .precompressed_gzip()
+                .precompressed_zstd(),
+        )
         .with_state(state);
 
     // run our app with hyper, listening globally on port 3000
@@ -43,5 +51,21 @@ async fn file_handler(
             Ok(res) => (StatusCode::OK, res),
             Err(e) => (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()),
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn test_file_handler() {
+        let state = Arc::new(PathState {
+            path: PathBuf::from("."),
+        });
+        let path = Path("Cargo.toml".to_string());
+        let (code, res) = file_handler(State(state), path).await;
+        assert_eq!(code, StatusCode::OK);
+        assert!(res.contains("[dependencies]"))
     }
 }
